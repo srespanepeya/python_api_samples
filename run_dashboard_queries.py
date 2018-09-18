1c317926b7023328fc5c459a75cd0d2882515931
Jesse Carah <jessecarah@Jesses-MacBook-Pro.local>
2018-09-18 13:14:08 -0400

    simplified query body, added support for looks

--- run_dashboard_queries.py
+++ run_dashboard_queries.py
@@ -24,8 +24,12 @@ looker = LookerApi(host=my_host,
                  secret = my_secret)
 
 dashboard = looker.get_dashboard(dashboard_id)
-dashboard_filters = []
+
+with open( 'dashboard.json', 'w') as f:
+    json.dump(dashboard, f, indent = 4)
+
 # Collect dashboard filters
+dashboard_filters = []
 for element in dashboard['dashboard_filters']:
     dashboard_filter = {}
     dashboard_filter['id'] = element['id']
@@ -34,12 +38,23 @@ for element in dashboard['dashboard_filters']:
     dashboard_filter['dashboard_id'] = element['dashboard_id']
     dashboard_filters.append(dashboard_filter)
 
+
+
 # Collect dashboard queries and associated filter listerners
+query_params = ['model', 'view', 'fields', 'pivots', 'filters', 'sorts', 'limit', 'total', 'row_total']
 dashboard_elements = []
 for element in dashboard['dashboard_elements']:
     dashboard_element = {}
-    dashboard_element['query'] = element['query']
-    del(dashboard_element['query']['client_id'])
+    # is element look or tile?
+    if element['look_id'] is None:
+        full_query = element['query']
+    else:
+        full_query = element['look']['query']
+
+    # keep the query params we care about
+    query = {param: full_query[param] for param in query_params}
+    dashboard_element['query'] = query
+
     dashboard_element['filter_listeners'] = [filterable['listen'] for filterable
                     in element['result_maker']['filterables']][0]
     dashboard_elements.append(dashboard_element)
@@ -54,19 +69,11 @@ for element in dashboard_elements:
                                 filter_listener['dashboard_filter_name'],
                                     dashboard_filters))[0]['default_value']
         filter_listener['default_value'] = default_value
-
-# apply dashboard filters to queries with default values
-for element in dashboard_elements:
-    # initialize filters object if it doesn't exist
+    # apply dashboard filters to queries with default values
     if element['query']['filters'] is None:
         element['query']['filters'] = {}
     for filter_listener in element['filter_listeners']:
         field = filter_listener['field']
         value = filter_listener['default_value']
         element['query']['filters'][field] = value
-    # pprint(looker.run_inline_query(element['query']))
-    print(json.dumps((element['query']), indent = 4))
-
-
-
-# pprint(looker.run_inline_query(e))
+    looker.run_inline_query(element['query'])
