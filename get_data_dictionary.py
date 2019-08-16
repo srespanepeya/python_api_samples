b94d933866741f1489184db104ba57a88e7e6997
Santiago Respane <srespane@gmail.com>
2019-08-16 15:29:15 -0300

    Ajuste para generacion de csv

--- get_data_dictionary.py
+++ get_data_dictionary.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import yaml
-from lookerapi import LookerApi
+from peyalookerapi import PeyaLookerApi
 from pprint import pprint as pp
 import json
 import csv
@@ -8,7 +8,7 @@ import requests
 
 ### ------- HERE ARE PARAMETERS TO CONFIGURE -------
 
-host = 'localhost'
+host = 'peya-looker'
 
 ### ------- OPEN THE CONFIG FILE and INSTANTIATE API -------
 
@@ -20,43 +20,65 @@ my_host = params['hosts'][host]['host']
 my_secret = params['hosts'][host]['secret']
 my_token = params['hosts'][host]['token']
 
-looker = LookerApi(host=my_host,
-                 token=my_token,
-                 secret = my_secret)
+looker = PeyaLookerApi(host=my_host,token=my_token,secret = my_secret)
 
 
 ## --------- csv writing -------
 
-def write_fields(explore, fields, model_name =""):
+def write_fields(explore, model_name =""):
+# --------- csv formatting -------------
+	csvfile= open('dictionary.csv', 'w',newline='')
+	w = csv.writer(csvfile, delimiter='|', quotechar='"', quoting=csv.QUOTE_ALL)
+	header = ['connection_name',
+				'field_type',
+				'project',
+				'model',
+				'explore',
+				'view',
+				'view_label',
+				'name',
+				'hidden',
+				'label',
+				'label_short',
+				'description',
+				'sql',
+				'ftype',
+				'value_format',
+				'source']
+	w.writerow(header)
 
 	### First, compile the fields you need for your row
-
-	explore_fields=explore['fields']
+	explore_name = explore.name
+	explore_model_name = model_name
+	explore_fields=explore.fields
+	WINDOWS_LINE_ENDING = '\r\n'
+	UNIX_LINE_ENDING = '\n'
 	try:
-		connection_name = str(explore['connection_name'])
+		connection_name = str(explore.connection_name)
 	except:
 		connection_name = ''
-	for dimension in explore_fields[fields]:
-		# print dimension
+	
+	for dimension in explore_fields.dimensions:
+		#print(dimension)
 
-		field_type = fields
-		project = str(dimension['project_name'])
-		explore = str(explore_def['name'])
-		model = str(model_name)
-		view=str(dimension['view'])
-		view_label=str(dimension['view_label'])
-		name=str(dimension['name'])
-		hidden=str(dimension['hidden'])
-		label=str(dimension['label'])
-		label_short=str(dimension['label_short'])
-		description=str(dimension['description'])
-		sql=str(dimension['sql'])
-		ftype=str(dimension['type'])
-		value_format=str(dimension['value_format'])
-		source = str(dimension['source_file'])
+		field_type = "Dimension"
+		project = str(dimension.project_name)
+		explore = str(explore_name)
+		model = str(explore_model_name)
+		view=str(dimension.view)
+		view_label=str(dimension.view_label)
+		name=str(dimension.name)
+		hidden=str(dimension.hidden)
+		label=str(dimension.label)
+		label_short=str(dimension.label_short)
+		description=str(dimension.description)
+		sql=str(dimension.sql)
+		ftype=str(dimension.type)
+		value_format=str(dimension.value_format)
+		source = str(dimension.source_file)
 
-	### compile the line - this is possible to combine above, but here to keep things simple
-		rowout = []
+		### compile the line - this is possible to combine above, but here to keep things simple
+		rowout = []	
 		rowout.append(connection_name)
 		rowout.append(field_type)
 		rowout.append(project)
@@ -69,60 +91,75 @@ def write_fields(explore, fields, model_name =""):
 		rowout.append(label)
 		rowout.append(label_short)
 		rowout.append(description)
-		rowout.append(sql)
+		rowout.append(sql.replace(WINDOWS_LINE_ENDING,' ').replace(UNIX_LINE_ENDING,' ').replace('\"',"\\\\"))
 		rowout.append(ftype)
 		rowout.append(value_format)
 		rowout.append(source)
-
+		w.writerow(rowout)
+	
+	for dimension in explore_fields.measures:
+    	#print(measuire)
+		field_type = "Measures"
+		project = str(dimension.project_name)
+		explore = str(explore_name)
+		model = str(explore_model_name)
+		view=str(dimension.view)
+		view_label=str(dimension.view_label)
+		name=str(dimension.name)
+		hidden=str(dimension.hidden)
+		label=str(dimension.label)
+		label_short=str(dimension.label_short)
+		description=str(dimension.description)
+		sql=str(dimension.sql)
+		ftype=str(dimension.type)
+		value_format=str(dimension.value_format)
+		source = str(dimension.source_file)
+		
+		### compile the line - this is possible to combine above, but here to keep things simple
+		rowout = []		
+		rowout.append(connection_name)
+		rowout.append(field_type)
+		rowout.append(project)
+		rowout.append(model)
+		rowout.append(explore)
+		rowout.append(view)
+		rowout.append(view_label)
+		rowout.append(name)
+		rowout.append(hidden)
+		rowout.append(label)
+		rowout.append(label_short)
+		rowout.append(description)
+		rowout.append(sql.replace(WINDOWS_LINE_ENDING,' ').replace(UNIX_LINE_ENDING,' ').replace('\"',"\\\\"))
+		rowout.append(ftype)
+		rowout.append(value_format)
+		rowout.append(source)
 		w.writerow(rowout)
 
-## --------- csv formatting -------------
-
-csvfile= open('dictionary.csv', 'w')
-
-w = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-header = ['connection_name',
-			'field_type',
-			'project',
-			'model',
-			'explore',
-			'view',
-			'view_label',
-			'name',
-			'hidden',
-			'label',
-			'label_short',
-			'description',
-			'sql',
-			'ftype',
-			'value_format',
-			'source']
-
-w.writerow(header)
-
-## --------- API Calls -------------
+# ## --------- API Calls -------------
 
 ## -- Get all models --
-models = looker.get_model("")
-pp(models)
+models = looker.get_models()
 for model in models:
-	model_name = model['name']
+	model_name = model.name
 
-	## -- Get single model --
-	model_def = looker.get_model(model_name)
-	# pp(model_def)
+	if model_name == "prueba_Gitte":
+    		
+    		
+		print("-----------------------------------------------")
+		#print(model)
+		print(model_name)
 
-	## -- Get single explore --
-	for explore_def in model_def['explores']:
-		explore=looker.get_explore(model_name, explore_def['name'])
-		# pp(explore)
-		## -- parse explore --
-
-		try:
-			write_fields(explore,'measures', model_name)
-		except:
-			print('Problem measure fields in ' + explore_def['name'])
-		try:
-			write_fields(explore,'dimensions', model_name)
-		except:
-			print('Problem dimension fields in ' + explore_def['name'])
+		for explore in model.explores:
+			#print(explore)
+			explore_name = explore.name
+			if explore_name =="fact_payment":
+				print("--->Model:{0}---Explore:{1}".format(model_name,explore_name))
+				super_explore = looker.get_model_explore(model_name,explore_name)
+			 # try:
+				write_fields(super_explore, model_name)
+			# # except:
+			# # 	print('Problem measure fields in ' + super_explore.name)
+			# # try:
+			# # 	write_fields(explore,'dimensions', model_name)
+			# # except:
+			# # 	print('Problem dimension fields in ' + explore_def['name'])
